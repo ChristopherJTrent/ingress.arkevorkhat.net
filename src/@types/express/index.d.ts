a3eb8f346376bdef4087df7f4330d5c70a75aad2
Christopher Trent <ChristopherJTrent@outlook.com>
2026-04-27 15:00:53 -0700

    fix incorrect type

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -1,8 +1,8 @@
 declare global {
     namespace Express {
         interface Request {
-            serviceName: String?,
-            serviceRequestPath: String?
+            serviceName: string?,
+            serviceRequestPath: string?
         }
     }
 }
