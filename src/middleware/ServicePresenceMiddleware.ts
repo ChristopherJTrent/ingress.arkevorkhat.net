e912e158f631027a7004acbe6ffb472ae6ff995b
Christopher Trent <ChristopherJTrent@outlook.com>
2026-04-29 14:58:09 -0700

    build basic service presence middleware

--- src/middleware/ServicePresenceMiddleware.ts
+++ src/middleware/ServicePresenceMiddleware.ts
@@ -1,19 +1,39 @@
 import type { Response, Request, NextFunction } from 'express';
 import {prisma} from '../common/prisma'
+import { matchRoute } from '../common/RouteMatcher';
+
+function sanitizeRequestPath(requestPath: string | null): string {
+	if (/\?.*\//.exec(requestPath??"") != null) {
+		throw "Illegal Argument, a slash cannot follow a question mark"
+	}
+	return requestPath?.split("?")[0] ?? ""
+}
+
 export async function ServicePresenceMiddleware(req: Request, res: Response, next: NextFunction) {
 	if (req.serviceName == null) {
 		next()
 		return
 	}
+
 	const service = await prisma.service.findFirst({
 		where: {
 			name: req.serviceName
+		},
+		include: {
+			routes: true
 		}
 	})
 	if (service == undefined) {
 		res.status(404).end()
 	}
 	else {
-		next()
+		const matched = service.routes
+			.filter((v) => matchRoute(v.path, sanitizeRequestPath(req.serviceRequestPath)))
+			.length > 0
+		if (matched) {
+			next()
+			return
+		}
+		res.status(404).end()
 	}
 }
