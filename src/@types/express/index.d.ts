1f34942b0cbdc5540de204870bdd862b75f5c62b
Christopher Trent <ChristopherJTrent@outlook.com>
2026-05-11 15:03:18 -0700

    update the request object to use optionals

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -1,11 +1,17 @@
-import type { Route, Service } from '../../../generated/prisma/client'
+import type { Prisma, Route, Service, User } from '../../../generated/prisma/client'
+import type { DefaultPrismaClient } from '../../../generated/prisma/internal/prismaNamespace'
+import type { Optional } from '../../common/Optional'
 
+type UserWithRoles = Prisma.UserGetPayload<{include: {roles: true}}>
+type RouteWithRoles = Prisma.RouteGetPayload<{include: {allowedRoles: true}}>
 declare global {
     namespace Express {
         interface Request {
-            serviceName: string?,
-            serviceRequestPath: string?,
-			routeObject: Route?
+			apiKey: Optional<string>,
+            serviceName: Optional<string>,
+            serviceRequestPath: Optional<string>,
+			routeObject: Optional<RouteWithRoles>,
+			user: Optional<UserWithRoles>
         }
     }
 }
