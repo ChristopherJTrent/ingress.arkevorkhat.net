e17c4a9d18aa761df8b717c81a9393dd1e1776c8
Christopher Trent <ChristopherJTrent@outlook.com>
2026-05-12 12:52:51 -0700

    migrate to prismalibsql

--- src/common/prisma.ts
+++ src/common/prisma.ts
@@ -1,8 +1,8 @@
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3"
+import { PrismaLibSql } from '@prisma/adapter-libsql'
 import { PrismaClient } from "../../generated/prisma/client"
 import "dotenv/config"
 
 const connectionString = `${process.env.DATABASE_URL}`
 
-const adapter = new PrismaBetterSqlite3({url: connectionString})
+const adapter = new PrismaLibSql({url: connectionString})
 export const prisma = new PrismaClient({adapter})
