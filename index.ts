2def14b69f4fe885e7fd5c5c40558e578363edf5
Christopher Trent <ChristopherJTrent@outlook.com>
2026-05-12 12:53:09 -0700

    setup express

--- index.ts
+++ index.ts
@@ -1,20 +1,26 @@
+import { getLogger } from '@logtape/logtape';
+import { configureLogger } from './src/common/logger';
 import { prisma } from "./src/common/prisma";
+import { app } from './src/router/ExpressApplication';
+import { router } from './src/router/RootRouter';
+import * as express from 'express'
 
 async function main() {
-    const user = await prisma.user.create({
-        data: {
-            username:"testUser",
-            password_hash: "invalid",
-        }
-    })
-    console.log("created user: ", user)
+	await configureLogger();
+	const logger = getLogger(['ingress.arkevorkhat.net', 'moduleMain'])
+	logger.debug `logger configured successfully\n`
     const allUsers = await prisma.user.findMany({
         include: {
             apiKeys: true
         }
     })
-    console.log("All Users: ", JSON.stringify(allUsers))
+    console.log("All Users: ", allUsers)
+	const PORT = process.env.LISTEN_PORT ?? 8080
+	app.listen(PORT, () => {
+		logger.info `express is running on port ${PORT}`
+	});
 }
+
 
 main().then(async () => {
     await prisma.$disconnect()
