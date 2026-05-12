6780b400fbde4cecc051f190e39cc44accb30991
Christopher Trent <ChristopherJTrent@outlook.com>
2026-05-12 15:04:11 -0700

    implement first time setup

--- index.ts
+++ index.ts
@@ -4,17 +4,60 @@ import { prisma } from "./src/common/prisma";
 import { app } from './src/router/ExpressApplication';
 import { router } from './src/router/RootRouter';
 import * as express from 'express'
+import { ApiKeyFactory } from './src/common/ApiKeyFactory';
+import { exit } from 'process';
 
 async function main() {
 	await configureLogger();
 	const logger = getLogger(['ingress.arkevorkhat.net', 'moduleMain'])
 	logger.debug `logger configured successfully\n`
-    const allUsers = await prisma.user.findMany({
-        include: {
-            apiKeys: true
-        }
-    })
-    console.log("All Users: ", allUsers)
+	let rootUser
+	if (await prisma.apiKey.count() < 1) {
+		let adminRole
+		if (await prisma.role.count() < 1) {
+			logger.debug `admin role not found, creating...`
+			adminRole = await prisma.role.create({
+				data: {
+					fqn: 'admin',
+					description: 'Administrator role, bypasses authorization entirely. Do not give this to users unless you want them to have full access.'
+				}
+			})
+		} else {
+			adminRole = await prisma.role.findFirst({
+				where: {
+					fqn: 'admin'
+				}
+			})
+		}
+		logger.debug `admin role found ${adminRole}`
+		if (await prisma.user.count() < 1) {
+			logger.debug `root user not found, creating...`
+			rootUser = await prisma.user.create({
+				data: {
+					username: 'root',
+					password_hash: 'LOGIN_NOT_PERMITTED',
+					roles: {
+						connect: {
+							fqn: 'admin'
+						}
+					}
+				}
+			})
+			logger.debug `root user created: ${rootUser}`
+		} else {
+			rootUser = await prisma.user.findFirst({
+				where: {username: 'root'},
+				include: {roles: true}
+			})
+		}
+		if (rootUser == undefined || rootUser == null) {
+			logger.fatal `root user is unexpectedly null. closing...`
+			exit(1)
+		}
+		const keyFactory = new ApiKeyFactory(rootUser);
+		const key = await keyFactory.get()
+		logger.debug`generated firstrun api key ${key}`
+	}
 	const PORT = process.env.LISTEN_PORT ?? 8080
 	app.listen(PORT, () => {
 		logger.info `express is running on port ${PORT}`
