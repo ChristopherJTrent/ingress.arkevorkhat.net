a2dc11dbcbe651b3309d0e8ff4f8e6d7667db8d4
Christopher Trent <ChristopherJTrent@outlook.com>
2026-05-12 12:51:42 -0700

    update authorization tests to check edge case

--- src/common/Authorization.test.ts
+++ src/common/Authorization.test.ts
@@ -2,7 +2,8 @@ import {expect, test} from 'vitest'
 import { matchRolesByName } from './Authorization'
 test.for([
 	{allowedRoles: ["admin"], userRoles: ["admin"], expected: true},
-	{allowedRoles: ['admin.apiKey'], userRoles: ['admin'], expected: true}
+	{allowedRoles: ['admin.apiKey'], userRoles: ['admin'], expected: true},
+	{allowedRoles: ['admin', 'user.create', 'user.update'], userRoles: ['apiKey.issue', 'filesystem.modify', 'media.download', 'user.create'], expected: true}
 ])('matchRolesByName($self, $other) -> $expected', ({allowedRoles, userRoles}) => {
 	expect(matchRolesByName(allowedRoles, userRoles)).toBe(true)
 })
