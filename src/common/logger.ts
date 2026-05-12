ae22186e9c7a333c7748151d3fc76fa49dbf12f6
Christopher Trent <ChristopherJTrent@outlook.com>
2026-05-12 15:04:21 -0700

    disable logtape default message

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -4,6 +4,7 @@ export async function configureLogger(): Promise<void> {
 	return configure({
 		sinks: {console: getConsoleSink()},
 		loggers: [
+			{category: ['logtape', 'meta'], sinks: ['console'], lowestLevel: 'warning'},
 			{category: 'ingress.arkevorkhat.net', lowestLevel: 'debug', sinks: ['console']}
 		]
 	})
