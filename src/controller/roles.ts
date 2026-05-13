2901238ac89b38320cbeef13b21e4fdd72d9461f
Christopher Trent <ChristopherJTrent@outlook.com>
2026-05-13 13:56:34 -0700

    add roles controller

--- src/controller/roles.ts
+++ src/controller/roles.ts
@@ -1,9 +1,32 @@
 import {Router, type Request, type Response} from 'express'
 import { prisma } from '../common/prisma';
+import type { Optional } from '../common/Optional';
+import { getLogger } from '@logtape/logtape';
+
+const logger = getLogger(['ingress.arkevorkhat.net', 'rolesController'])
+
 const router = Router();
 router.get('/', async (req, res) => {
 	const roles = await prisma.role.findMany()
 	res.json(roles).send();
+})
+router.post('/create', async (req, res) => {
+	try{
+		logger.debug `${req.body}`
+		const body = req.body as { fqn: string, description: string }
+		const created = await prisma.role.create({
+			data: {
+				fqn: body.fqn,
+				description: body.description
+			}
+		})
+		if (created != undefined) {
+			res.status(201).end().send()
+		}
+	} catch (e) {
+		logger.debug `error: ${e}`
+		res.status(500).json(e)
+	}
 })
 
 export {router as rolesController}
--- src/router/RootRouter.ts
+++ src/router/RootRouter.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import { rolesController } from '../controller/roles';
 
 const router: Router = Router()
+
+router.use('/roles', rolesController)
 
 export {router}
